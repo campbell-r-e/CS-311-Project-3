0fecd2e175f015ac576fbb85c9ccdf3c5cefecff
bsucs <122378640+bsucs@users.noreply.github.com>
2024-11-29 14:09:41 -0500

    added insert

---
+++ project3/src/pages/api/insertcookingstep.ts
@@ -0,0 +1,39 @@
+
+
+
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const prisma = new PrismaClient();
+
+
+export default async function handler(req:NextApiRequest,res: NextApiResponse) {
+  try{
+    const { ingredientname} = req.body;
+    
+    const datainsert = await prisma.cookingStep.create({
+      data: {
+
+        description: ingredientname,
+        order: null,                  
+        recipeId: undefined,
+        
+      },
+      
+    });
+  
+    res.status(200).json(datainsert);// from online 
+    
+    
+    
+
+    
+    
+  }catch(error){
+    res.status(500).json({ error: "Failed to fetch questions" });
+    console.log(error)
+  }
+
+
+}
+// modified from a online example
--- project3/src/pages/insert_steps/stepinsert.tsx
+++ project3/src/pages/insert_steps/stepinsert.tsx
@@ -2,21 +2,50 @@ import { useState } from 'react';
 
 
 
-export default function Methods() {
-  const [methods, setmethods] = useState("");
+export default function steps() {
+  const [steps, setsteps] = useState("");
 
   const handleSubmit = (event:React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    alert(`input accepted`)
+    updates();
   }
+  async function updates(){
+
+ 
+    
+ 
+    try {
+      const response = await fetch("/api/insertcookingsteps", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ 
+          description:steps,
+          
+
+        }),
+      });
+  
+      if (!response.ok) {
+        throw new Error("Failed to update");
+      }
+  
+      const data = await response.json();
+      console.log("Update response:", data);
+      setsteps("")
+      
+    } catch (error) {
+      console.error("Error updating amount understood:", error);
+    }}
 
   return (
     <form onSubmit={handleSubmit}>
       <label> Enter a cooking step as a template string in this format: &quot;beat [ingredient] for 10 minutes.&quot:
         <input 
           type="text" 
-          value={methods}
-          onChange={(e) => setmethods(e.target.value)}
+          value={steps}
+          onChange={(e) => setsteps(e.target.value)}
         />
       </label>
 
