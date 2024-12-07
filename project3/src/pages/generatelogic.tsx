0c382032c21f80a0b00c4e6b7e4ab4a14989fc63
bsucs <122378640+bsucs@users.noreply.github.com>
2024-12-07 00:21:14 -0500

    added code

--- project3/src/pages/generatelogic.tsx
+++ project3/src/pages/generatelogic.tsx
@@ -2,8 +2,57 @@
 
 import React, { useEffect, useState } from "react";
 
+export interface ColourOption {
+  value: string;
+   label: string;
+ 
+  
+ }
+ export interface tags{
+  name:string;
+  
+ }
+ 
+ async function fetchColourOptions(): Promise<ColourOption[]> {
+   try {
+     const response = await fetch('/api/options');
+     if (!response.ok) {
+       throw new Error("Network response was not ok");
+     }
+     const data: tags[] = await response.json();
+
+  
+     return data.map(tag => ({
+       value: tag.name, 
+       label: tag.name,
+      
+   }));
+   
+   } catch (error) {
+     console.error("Failed to fetch questions:", error);
+     return [];
+    
+   }
+ }
+ export function useColourOptions() {
+   const [colourOptions, setColourOptions] = useState<ColourOption[]>([]);
+
+   useEffect(() => {
+     async function loadOptions() {
+       const options = await fetchColourOptions();
+       setColourOptions(options || []);
+     }
+ 
+     loadOptions();
+   }, []);
+ 
+   return colourOptions;
+ }
+ 
+
 
 const App = () => {
+  
   
 
   return (
